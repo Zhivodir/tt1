f410f94a5dc42a452d76fc305a8186a232e2d87f
zhivodir <dzhivchik@gmail.com>
2019-08-21 23:59:34 +0300

    Test task

--- src/FunctionRealisation.java
+++ src/FunctionRealisation.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class FunctionRealisation {
     static void order(List<Person> persons, int[] ids) {
@@ -6,25 +7,45 @@ public class FunctionRealisation {
     }
 
     static private List<Person> process(List<Person> persons, int[] ids) {
-        if(persons == null || ids == null){
+        if (persons == null || ids == null) {
             throw new IllegalArgumentException();
         }
 
-        persons = new ArrayList<>(persons);
-        int currentPersonsIndex = 0;
+        Map<Integer, Person> mapOfPersons = persons.stream().collect(Collectors.toMap(Person::getId, person -> person, (x, y) -> y, LinkedHashMap::new));
+        List<Person> result = new ArrayList<>();
 
         for (int i = 0; i < ids.length; i++) {
             int needPersonId = ids[i];
-            for (int personsIndex = currentPersonsIndex; personsIndex < persons.size(); personsIndex++) {
-                Person personForChecking = persons.get(personsIndex);
-                if (needPersonId == personForChecking.getId()) {
-                    persons.remove(personsIndex);
-                    persons.add(currentPersonsIndex, personForChecking);
-                    currentPersonsIndex++;
-                    break;
-                }
+            if (mapOfPersons.containsKey(needPersonId)) {
+                Person person = mapOfPersons.remove(needPersonId);
+                result.add(person);
             }
         }
-        return persons;
+
+        result.addAll(mapOfPersons.values().stream().collect(Collectors.toList()));
+        return result;
     }
+
+//    static private List<Person> process(List<Person> persons, int[] ids) {
+//        if(persons == null || ids == null){
+//            throw new IllegalArgumentException();
+//        }
+//
+//        persons = new ArrayList<>(persons);
+//        int currentPersonsIndex = 0;
+//
+//        for (int i = 0; i < ids.length; i++) {
+//            int needPersonId = ids[i];
+//            for (int personsIndex = currentPersonsIndex; personsIndex < persons.size(); personsIndex++) {
+//                Person personForChecking = persons.get(personsIndex);
+//                if (needPersonId == personForChecking.getId()) {
+//                    persons.remove(personsIndex);
+//                    persons.add(currentPersonsIndex, personForChecking);
+//                    currentPersonsIndex++;
+//                    break;
+//                }
+//            }
+//        }
+//        return persons;
+//    }
 }
